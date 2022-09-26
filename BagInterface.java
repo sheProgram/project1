a7a5332497ffb38f052ef15255cb8850d4403a49
tiff178 <tiffmt817@gmail.com>
2022-09-25 19:36:10 -0700

    Add union into BagInterface, LinkedBag, & ResizableArrayBag, update BagDriver to check if union of two bags works

--- BagInterface.java
+++ BagInterface.java
@@ -46,4 +46,10 @@ public interface BagInterface<T>
 		 @return  A newly allocated array of all the entries in the bag.
                 Note: If the bag is empty, the returned array is empty. */
 	public T[] toArray();
+
+	/** Retrieves entries from BagDriver.Java
+	 * @param bag2
+	 * @return
+	 */
+	public BagInterface<T> union(BagInterface<T> bag2);
 } // end BagInterface
